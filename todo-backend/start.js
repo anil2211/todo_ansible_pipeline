7909d5eff55c49709cec3c22070b115ac2b18ee1
anil <anil@gmail.com>
2025-05-05 10:41:28 +0530

    code added

--- todo-backend/start.js
+++ todo-backend/start.js
@@ -1,6 +1,6 @@
 const app = require("./server")
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT,()=>{
     console.log(`server is running on the port ${PORT}`)
 })
