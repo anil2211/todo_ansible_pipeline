4d59ce31b51fee661821320387433e9418ae1d2b
anil <anil@gmail.com>
2025-05-05 11:27:17 +0530

    code added

--- todo-backend/db.js
+++ todo-backend/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose")
 const logger = require("./utils/loggers")
 
+MONGO_URI="mongodb+srv://anil:anil2211@cluster0.cqvrpoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 const connectDB = async()=>{
     try{
-    await mongoose.connect(process.env.MONGO_URI)
+    // await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(MONGO_URI)
     // console.log("mongoDb connected")
     logger.info("mongoDb connected")
     } catch(error){
--- todo-backend/start.js
+++ todo-backend/start.js
@@ -1,6 +1,7 @@
 const app = require("./server")
 
-const PORT = process.env.PORT || 3002;
+// const PORT = process.env.PORT || 3002;
+const PORT = 3002;
 app.listen(PORT,()=>{
     console.log(`server is running on the port ${PORT}`)
 })
